75107fc691f6fdcdafa5e013da4f419cdb985de7
downloadtheworld <knelmes@gmail.com>
2020-06-20 21:41:35 +0100

    Update Get-BuildInstructions.ps1

--- Get-BuildInstructions.ps1
+++ Get-BuildInstructions.ps1
@@ -8,16 +8,20 @@ $index = Import-Csv $output
 foreach ($file in $index){
     $url = $file.url
     $output = $outputfolder + $file.setnumber + '.pdf'
-    Write-Host "Trying " $file.SetNumber - $file.Description " - " $file.Notes
-    try{
-        Invoke-WebRequest -Uri $url -OutFile $output
-    } 
-    catch{
-        switch ($_.Exception.Response.StatusCode.Value__)
-        {
-            403{ 
-                Write-Host -ForegroundColor Yellow "Access Denied - Probably not on Lego's servers anymore."           
+    if(!(Test-Path $output)){
+        Write-Host "Trying " $file.SetNumber - $file.Description " - " $file.Notes
+        try{
+            Invoke-WebRequest -Uri $url -OutFile $output
+        } 
+        catch{
+            switch ($_.Exception.Response.StatusCode.Value__)
+            {
+                403{ 
+                    Write-Host -ForegroundColor Yellow "Access Denied - Probably not on Lego's servers anymore."           
+                }
             }
         }
+    }else{
+        Write-Host -ForegroundColor Green $file.SetNumber " already exists, skipping"
     }
 }
