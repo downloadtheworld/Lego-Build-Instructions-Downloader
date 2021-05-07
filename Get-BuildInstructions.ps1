5e8840b2dcfb23488cbc3b7295dffd9ebfc645a5
downloadtheworld <knelmes@gmail.com>
2021-05-07 22:04:17 +0100

    Update Get-BuildInstructions.ps1
    remove error checking and enable multiple downloads for sets

--- Get-BuildInstructions.ps1
+++ Get-BuildInstructions.ps1
@@ -3,25 +3,16 @@ $outputfolder = "e:\lego\"
 $output = $outputfolder + 'index.csv'
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $url -OutFile $output
+#Invoke-WebRequest -Uri $url -OutFile $output
 $index = Import-Csv $output
 foreach ($file in $index){
     $url = $file.url
-    $output = $outputfolder + $file.setnumber + '.pdf'
+    $desc = $file.description.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+    $output = $outputfolder + $file.setnumber + ' ' + $desc +  '.pdf'
     if(!(Test-Path $output)){
-        Write-Host "Trying" $file.SetNumber - $file.Description " - " $file.Notes
-        try{
-            Invoke-WebRequest -Uri $url -OutFile $output
-        } 
-        catch{
-            switch ($_.Exception.Response.StatusCode.Value__)
-            {
-                403{ 
-                    Write-Host -ForegroundColor Yellow "Access Denied - Probably not on Lego's servers anymore."           
-                }
-            }
-        }
+        Write-Host "Trying" $file.SetNumber - $file.Description
+        Invoke-WebRequest -Uri $url -OutFile $output
     }else{
-        Write-Host -ForegroundColor Green $file.SetNumber "already exists, skipping"
+        Write-Host -ForegroundColor Green $file.SetNumber - $desc "already exists, skipping"
     }
 }
